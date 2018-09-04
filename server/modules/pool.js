2d377ef8a28741c3a55ae7c12e2eb96ef97f21c6
Deanthony Miller <dmil1301@protonmail.com>
2018-09-04 11:50:38 -0500

    fixing db added columns jobs, date, employer

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -30,11 +30,7 @@ if (process.env.DATABASE_URL) {
   config = {
     host: 'localhost', // Server hosting the postgres database
     port: 5432, // env var: PGPORT
-<<<<<<< HEAD
-    database: 'gildasClub', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
-=======
     database: 'gildasclub', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
->>>>>>> f2260b1b8920b2c84332e96d64b872f1e565320d
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
