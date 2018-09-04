d643e719f9db263217da40342978d87b1eecc85c
BJ Pennington <bjpennington112@gmail.com>
2018-09-04 12:03:22 -0500

    update header text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,8 @@ const Header = ({ title }) => (
   <div className="instructions">
     <div>
       <img src="/images/GCTC_Logo.jpg" alt="Gilda's Club Twin Cities Logo" height="150"/>
-      <h1 className="lead">{ title}</h1>
+      {/* <h1 className="lead">{ title}</h1> */}
+      <p>Life is about not knowing, having to change, taking the moment and making the best of it, without knowing what's going to happen next.</p>
     </div>
   </div>
 );
