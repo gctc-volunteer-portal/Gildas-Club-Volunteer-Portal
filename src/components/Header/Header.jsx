6b4ead065a5bca901934bf8b832b7c415f31002d
BJ Pennington <bjpennington112@gmail.com>
2018-09-04 11:55:14 -0500

    update header with logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Header = ({ title }) => (
   <div className="instructions">
     <div>
+      <img src="/images/GCTC_Logo.jpg" alt="Gilda's Club Twin Cities Logo" height="150"/>
       <h1 className="lead">{ title}</h1>
     </div>
   </div>
