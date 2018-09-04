4d3e4b5c55288174747500c25babcec7e4ea0283
Danny Yang <dannyyaaj@gmail.com>
2018-09-04 11:29:32 -0500

    import Material-UI, create textfields to collect user registration info

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,11 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
 
+import { withStyles, FormControl, FormLabel, TextField, Button } from '@material-ui/core';
 class RegistrationForm extends Component {
+
+  returnHome = () => {
+    this.props.history.push('/')
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <React.Fragment>
+        <form onSubmit={this.props.registerUser}>
+          <h1>Register User</h1>
+          <FormControl>
+            <TextField
+              label="Email"
+              type="text"
+              name="email"
+              value={this.props.userRegistrationInfo.email}
+              onChange={this.props.handleInputChangeFor('email')}
+            />
+            <TextField
+              label="Password"
+              type="password"
+              name="password"
+              value={this.props.userRegistrationInfo.password}
+              onChange={this.props.handleInputChangeFor('password')}
+            />
+            <Button
+              type="submit"
+              onClick={this.returnHome}
+              variant="raised"
+              color="primary"
+            >
+              Register
+              </Button>
+          </FormControl>
+        </form>
+      </React.Fragment>
     )
   }
 }
