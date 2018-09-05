f429845088ee0fac8999d4e83e2b4ba5bc101302
Deanthony Miller <dmil1301@protonmail.com>
2018-09-05 08:30:42 -0500

    finally got nav to spread using css grids

--- src/components/Nav/VolunteerNav/VolunteerNav.js
+++ src/components/Nav/VolunteerNav/VolunteerNav.js
@@ -10,7 +10,10 @@ import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
   nav:{
-    backgroundColor: '#DE2027'
+    backgroundColor: '#DE2027',
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr'
+    //  grid-column: 1 / 1;
   }
 }
 
