93d57521554f90d1194aab2cbed2170dfb4885eb
Danny Yang <dannyyaaj@gmail.com>
2018-09-06 13:38:14 -0500

    create certifications router

--- server/routes/certifications.router.js
+++ server/routes/certifications.router.js
@@ -1,24 +1,13 @@
 const express = require('express');
-const { rejectUnauthenticated } = require('../modules/authentication-middleware');
-const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
-const userStrategy = require('../strategies/user.strategy');
-
 const router = express.Router();
 
-// Handles Ajax request for user information if user is authenticated
-router.get('/', rejectUnauthenticated, (req, res) => {
-  // Send back user object from database
-  res.send(req.user);
-});
+// GET route for certifications to show up on create opportunities form
 
-// Handles POST request with new user data
-// The only thing different from this and every other post we've seen
-// is that the password gets encrypted before being inserted
-router.post('/', (req, res, next) => {
-  const userData = req.body
+router.get('/', (req, res, next) => {
+  const opportunityData = req.body
 
-  const queryText = ``;
+  const queryText = `SELECT * FROM "certifications";`;
 
   pool.query(queryText)
     .then((results) => {
@@ -26,22 +15,7 @@ router.post('/', (req, res, next) => {
     }).catch((error) => {
       console.log('Error on /api/certifications/ GET:', error);
       res.sendStatus(500);
-  });
-});
-
-// Handles login form authenticate/login POST
-// userStrategy.authenticate('local') is middleware that we run on this route
-// this middleware will run our POST if successful
-// this middleware will send a 404 if not successful
-router.post('/login', userStrategy.authenticate('local'), (req, res) => {
-  res.sendStatus(200);
-});
-
-// clear all server session information about this user
-router.get('/logout', (req, res) => {
-  // Use passport's built-in method to log out the user
-  req.logout();
-  res.sendStatus(200);
+    });
 });
 
 module.exports = router;
