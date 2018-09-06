ce0803ac5a9f6ab8d51f33b85abb260cb04cd8d9
Danny Yang <dannyyaaj@gmail.com>
2018-09-06 13:38:42 -0500

    add post route for new opportunities

--- server/routes/opportunities.router.js
+++ server/routes/opportunities.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 /**
  * GET route template
@@ -22,7 +23,7 @@ router.get('/:id', (req, res) => {
     LEFT OUTER JOIN "users" ON "users".id = "user_opportunities".user_id
     LEFT OUTER JOIN "opportunities" ON "opportunities".id = "user_opportunities".opportunity_id
     WHERE "user_opportunities"."opportunity_id" = $1;` ;
-    pool.query(queryText,[req.params.id])
+    pool.query(queryText, [req.params.id])
         .then((results) => {
             res.send(results.rows)
             console.log(results.rows);
@@ -36,8 +37,18 @@ router.get('/:id', (req, res) => {
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
-
+router.post('/', rejectUnauthenticated, (req, res) => {
+    const newOpportunity = req.body;
+    console.log(req.body, 'req body');
+    const queryText = ``;
+    const serializedData = []
+    pool.query(queryText, serializedData)
+        .then((results) => {
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            res.sendStatus(500);
+        })
 });
 
 module.exports = router;
