63093ec9546f6f54a4f5f6a7ed232bbdbc3ee76d
Danny Yang <dannyyaaj@gmail.com>
2018-09-07 17:06:52 -0500

    combine material-ui imports and create edit opportunity dialog

--- src/components/OpportunitiesCard_AdminView/OpportunitiesCard_AdminView.js
+++ src/components/OpportunitiesCard_AdminView/OpportunitiesCard_AdminView.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import {connect} from 'react-redux'
+import {
+    withStyles, Card, CardMedia, CardActionArea, CardContent,
+    CardActions, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Typography
+} from '@material-ui/core/';
+
+import { connect } from 'react-redux'
 import AdminManageVolunteersDialogue from '../AdminManageVolunteersDialogue/AdminManageVolunteersDialogue';
+import EditOpportunityForm from '../EditOpportunityForm/EditOpportunityForm';
 
 
 const styles = {
@@ -19,50 +17,94 @@ const styles = {
     media: {
         height: 350,
         width: '100%'
-
     },
+    dialog: {
+        textAlign: 'center',
+        height: '100vh',
+    }
 
 };
 
 class MediaCard extends Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            editEventIsOpen: false,
+            opportunityToUpdate: {},
+            opportunityId: '',
+        }
     }
 
-    componentDidMount(){
-        this.props.dispatch({type:'GET_EVENT_VOLUNTEERS', payload: this.props.opportunity.id})
+    componentDidMount() {
+        this.props.dispatch({ type: 'GET_EVENT_VOLUNTEERS', payload: this.props.opportunity.id })
     }
+
+    openEditOpportunity = (opportunityId, opportunityToUpdate) => {
+        this.setState({
+            editEventIsOpen: true,
+            opportunityId: opportunityId,
+            opportunityToUpdate: opportunityToUpdate,
+        })
+    }
+
+    closeEditOpportunity = () => {
+        this.setState({
+            editEventIsOpen: false,
+        });
+    };
+
     render() {
+        console.log(this.state, 'local state')
         const { classes } = this.props;
         return (
-            <Card className={classes.card}>
-                <CardActionArea>
-                    <CardMedia
-                        className={classes.media}
-                        image="https://www.gildasclubtwincities.org/wp-content/themes/skeleton/images/logo.png"
-                        title="Contemplative Reptile"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="headline" component="h2">
-                            {this.props.opportunity.title}
+            <React.Fragment>
+                <Card className={classes.card}>
+                    <CardActionArea>
+                        <CardMedia
+                            className={classes.media}
+                            image="https://www.gildasclubtwincities.org/wp-content/themes/skeleton/images/logo.png"
+                            title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="headline" component="h2">
+                                {this.props.opportunity.title}
+                            </Typography>
+                            <Typography component="p">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                across all continents except Antarctica
                         </Typography>
-                        <Typography component="p">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                            across all continents except Antarctica
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                        Share
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary">
+                            Share
                     </Button>
-                    <Button onClick={this.handleClickOpen}>Edit Opportunity</Button>
-                    <AdminManageVolunteersDialogue 
-                        opportunity = {this.props.opportunity}
+                        <Button
+                            onClick={() => this.openEditOpportunity(this.props.opportunity.id, this.props.opportunity)}
 
-                    />
-                </CardActions>
-            </Card>
+                        >Edit Opportunity</Button>
+                        <AdminManageVolunteersDialogue
+                            opportunity={this.props.opportunity}
+                        />
+                    </CardActions>
+                </Card>
+                <Dialog
+                    className={this.props.classes.dialog}
+                    aria-labelledby="edit a volunteer event"
+                    open={this.state.editEventIsOpen}
+                    onClose={this.handleCloseDialog}
+                >
+                    <DialogTitle>{"Edit Opportunity"}</DialogTitle>
+                    <DialogContent>
+                        <EditOpportunityForm
+                            eventId={this.state.eventId}
+                            eventToUpdate={this.state.eventToUpdate}
+                            closeEditOpportunity={this.closeEditOpportunity}
+                        />
+                    </DialogContent>
+                </Dialog>
+            </React.Fragment>
+
         );
     }
 }
