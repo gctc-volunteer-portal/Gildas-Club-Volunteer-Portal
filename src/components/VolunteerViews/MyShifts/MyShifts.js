bb1b0a9233a45c437e4d78500edd424f2a881fdf
petecoj <petecojohns@gmail.com>
2018-09-11 13:07:20 -0500

    added selector for opportunity status on manager opportunities page

--- src/components/VolunteerViews/MyShifts/MyShifts.js
+++ src/components/VolunteerViews/MyShifts/MyShifts.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import TextField from '@material-ui/core/TextField'
 import VolunteerNav from '../../Nav/VolunteerNav/VolunteerNav'
 import Header from '../../Header/Header';
 import OpportunitiesCard from '../../OpportunitiesCard_AdminView/OpportunitiesCard_AdminView';
-import VolunteerOpportunityDialog from '../VolunteerOpportunityDialog/VolunteerOpportunityDialog';
-
 import { USER_ACTIONS } from '../../../redux/actions/userActions'
 // import { triggerLogout } from '../../redux/actions/loginActions';
 
@@ -15,7 +13,23 @@ const mapStateToProps = state => ({
     shifts: state.opportunitiesReducer.singleVolunteerOpportunities
 });
 
+function searchingFor(term) {
+  return function (opportunity) {
+      if (opportunity.title) {
+          return opportunity.title.toLowerCase().includes(term.toLowerCase()) || !term;
+      }
+  }
+}
+
 class MyShifts extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+      term: '',
+  }
+  this.searchHandler = this.searchHandler.bind(this);
+}
+
   componentDidMount() {
     this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
     this.props.dispatch({ type: 'FETCH_SINGLE_VOLUNTEER_OPPORTUNITIES'})
@@ -25,11 +39,16 @@ class MyShifts extends Component {
       this.props.history.push('home');
     }
   }
+  searchHandler(event) {
+    this.setState({
+        term: event.target.value
+    })
+}
 
 
   render() {
 
-    let mappedMyShfits = this.props.shifts.map((shift, index) => {
+    let mappedMyShifts = this.props.shifts.filter(searchingFor(this.state.term)).map((shift, index) => {
       return (
         <OpportunitiesCard opportunity={shift} key={index} />
       )
@@ -38,7 +57,7 @@ class MyShifts extends Component {
     let content = null;
 
     if (this.props.user.email) {
-      content = mappedMyShfits;
+      content = mappedMyShifts;
     }
 
     return (
@@ -46,6 +65,21 @@ class MyShifts extends Component {
         <Header />
         <VolunteerNav />
         <h1>My Shift!!</h1>
+        <div style={{ height: 100 }}>
+                  <TextField
+                      id="full-width"
+                      label=""
+                      InputLabelProps={{
+                          shrink: true,
+                      }}
+                      placeholder="Search..."
+                      helperText="What opportunity are you looking for?"
+                      width='50'
+                      margin="normal"
+                      onChange={this.searchHandler}
+                      value={this.state.term}
+                  />
+              </div>
         { content }
       </div>
     );
