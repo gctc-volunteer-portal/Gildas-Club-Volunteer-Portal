aa3907864dc0ec2135ab0709aa0367f297ade033
BJ Pennington <bjpennington112@gmail.com>
2018-09-12 23:29:08 -0500

    allow users to reset password using a unique link

--- server/modules/nodemailer-password-reset.js
+++ server/modules/nodemailer-password-reset.js
@@ -1,3 +1,17 @@
 const nodemailer = require('nodemailer');
 
-let transporter = nodemailer.createTransport(transport[, defaults]);
+let transporter = nodemailer.createTransport({
+    host: 'smtp.google.com',
+    port: 465,
+    secure: true,
+    auth: {
+        type: 'OAuth2',
+        user: process.env.NODEMAILER_CLIENT_USER,
+        clientId: process.env.NODEMAILER_CLIENT_ID,
+        clientSecret: process.env.NODEMAILER_CLIENT_SECRET,
+        refreshToken: process.env.NODEMAILER_REFRESH_TOKEN,
+        accessToken: process.env.NODEMAILER_ACCESS_TOKEN,
+    }
+});
+
+module.exports = transporter;
