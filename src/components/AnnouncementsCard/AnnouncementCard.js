e86b3675ce524dccfc4fbaa172fdad05d49c5f6b
Deanthony Miller <dmil1301@protonmail.com>
2018-09-15 16:50:07 -0500

    finished got preview text working

--- src/components/AnnouncementsCard/AnnouncementCard.js
+++ src/components/AnnouncementsCard/AnnouncementCard.js
@@ -30,6 +30,9 @@ const styles = {
   pos: {
     marginBottom: 12,
   },
+  preview: {
+    maxWidth: '80%',
+  },
 };
 
 
@@ -60,17 +63,13 @@ class SimpleCard extends Component {
     <Card className={classes.card}>
       <CardContent>
         <Typography className={classes.title} color="textSecondary">
-         {moment(this.props.announcement.date).format("dddd, MMM D, YYYY")}
+        Date Posted: {moment(this.props.announcement.date).format("dddd, MMM D, YYYY")}
         </Typography>
         <Typography variant="headline" component="h2">
          {this.props.announcement.title}
-         <p className="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-         aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
-         deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-         velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+         <div className={this.props.classes.preview}>
+         <p className="p">{this.props.announcement.description}</p>
+         </div>
         </Typography>
         <Typography className={classes.pos} color="textSecondary">
         </Typography>
