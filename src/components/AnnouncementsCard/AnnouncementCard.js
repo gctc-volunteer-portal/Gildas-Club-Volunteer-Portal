03d192bd2e3b6fb8032b1fefd0107e0a8dc1e956
Deanthony Miller <dmil1301@protonmail.com>
2018-09-15 16:41:04 -0500

    got preview text working

--- src/components/AnnouncementsCard/AnnouncementCard.js
+++ src/components/AnnouncementsCard/AnnouncementCard.js
@@ -64,7 +64,13 @@ class SimpleCard extends Component {
         </Typography>
         <Typography variant="headline" component="h2">
          {this.props.announcement.title}
-         <p className="p">helloooooo d ahsdh asjhd jhas dhjasjhd hasj dhj asjhd hjasd</p>
+         <p className="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
+         aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+         deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+         velit esse cillum dolore eu
+          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
         </Typography>
         <Typography className={classes.pos} color="textSecondary">
         </Typography>
